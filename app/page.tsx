8f8670ff4adf88b6581699e0d1b154a855b01c76
Ayush Patil <aayushpatil558321@gmail.com>
2023-10-11 00:16:14 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,84 @@
+"use client";
 import Image from 'next/image'
+import { useEffect, useRef } from 'react';
+import P1 from '~/assets/images/p1.webp';
+import P2 from '~/assets/images/p2.webp';
+import P3 from '~/assets/images/p3.webp';
 
 export default function Home() {
+  const section1 = useRef<HTMLDivElement>(null);
+  const section2 = useRef<HTMLDivElement>(null);
+  const section3 = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+
+    (async () => {
+
+      const calculateScrollPercentage = (rect: DOMRect) => {
+        const scrollTop = window.scrollY || document.documentElement.scrollTop;
+        const elementRect = rect;
+        const percentage = ((scrollTop - elementRect.top) / elementRect.height) * 100;
+        const clampedPercentage = Math.max(0, Math.min(100, percentage));
+        
+        return clampedPercentage
+      };
+
+      const anime = (await import("animejs/lib/anime.es.js")).default;
+
+      const scaleDownSection1 = anime({
+        targets: '#section1',
+        scale: 0.5,
+        easing: 'easeInOutSine',
+        autoplay: false,
+        duration: 400
+      })
+
+      const scaleDownSection2 = anime({
+        targets: '#section2',
+        scale: 0.5,
+        easing: 'easeInOutSine',
+        autoplay: false,
+        duration: 400
+      })
+
+      const scaleDownSection3 = anime({
+        targets: '#section3',
+        scale: 0.5,
+        easing: 'easeInOutSine',
+        autoplay: false,
+        duration: 400
+      })
+
+      window.onscroll = function () {
+        scaleDownSection1.seek(calculateScrollPercentage(section1.current!.getBoundingClientRect()))
+        scaleDownSection2.seek(calculateScrollPercentage(section3.current!.getBoundingClientRect()))
+      }
+
+    })()
+
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="flex justify-center items-center">
+        <div className='h-min max-w-[1480px] w-full px-8 flex justify-center items-center flex-col'>
+            <div className="h-screen sticky w-full top-0" ref={section1} id='section1'>
+              <div className='h-[90%] absolute left-[calc(50.00000000000002%_-_100%/2)] top-[calc(50.00000000000002%_-_90%/2)] w-full'>
+                <Image src={P1} alt='beach' className='rounded-2xl h-full w-full object-cover object-center' />
+              </div>
+            </div>
+            <div className="h-screen sticky w-full top-0" ref={section2} id='section2'>
+              <div className='h-[90%] absolute left-[calc(50.00000000000002%_-_100%/2)] top-[calc(50.00000000000002%_-_90%/2)] w-full'>
+                <Image src={P2} alt='beach' className='rounded-2xl h-full w-full object-cover object-center' />
+              </div>
+            </div>
+            <div className="h-screen sticky w-full top-0" ref={section3} id='section3'>
+              <div className='h-[90%] absolute left-[calc(50.00000000000002%_-_100%/2)] top-[calc(50.00000000000002%_-_90%/2)] w-full'>
+                <Image src={P3} alt='beach' className='rounded-2xl h-full w-full object-cover object-center' />
+              </div>
+            </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   )
 }
